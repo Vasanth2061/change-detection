84c98855aca384514231111edf6100640d98bc01
Vasanth M <vasanth.m@takeda.com>
2024-11-08 11:03:09 +0530

    using rxjs

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, NgZone, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, NgZone, OnChanges, signal } from '@angular/core';
 
+import { OnInit } from '@angular/core';
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
 @Component({
@@ -8,21 +9,22 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   count = signal(0);
 
   private zone = inject(NgZone);
 
-  constructor() {
-    setTimeout(()=> {
+  ngOnInit() {
+    setInterval(()=> {
       this.count.set(0);
     }, 4000);
 
     this.zone.runOutsideAngular(() => {
       setInterval(() => {
         console.log('Timer expired');
-      }, 1000);
+      }, 5000);
     });
 
   }
